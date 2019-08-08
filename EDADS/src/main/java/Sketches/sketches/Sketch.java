84aba3e5b1b752f295561e9b0de45ad529f477a0
jvonhein <44237928+jvonhein@users.noreply.github.com>
2019-08-08 16:54:04 +0200

    Add files via upload

---
+++ EDADS/src/main/java/Sketches/sketches/Sketch.java
@@ -0,0 +1,27 @@
+package Sketches.sketches;
+
+import org.apache.flink.annotation.Internal;
+
+/**
+ * Generic interface for a Sketch.
+ *
+ * @param <T> the Input Type
+ * @param <R> the result type of the query
+ */
+@Internal
+public interface Sketch<T, R> extends java.io.Serializable{
+
+	/**
+	 * Update the sketch with a value T
+	 * @param t
+	 */
+	void update(T t);
+
+	/**
+	 * Query the sketch and get a Return Value
+	 * @param t
+	 * @return Value
+	 */
+	R query(T t);
+
+}
