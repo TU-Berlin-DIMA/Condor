da5750372353b699b38c9e9b7ba8ce7dbd472373
rudip7 <rudipoepsel7rpl@hotmail.com>
2019-08-16 17:06:56 +0200

    Count Windows also work

--- src/main/java/Jobs/CountWindowJob.java
+++ src/main/java/Jobs/CountWindowJob.java
@@ -1,13 +1,16 @@
 package Jobs;
 
+import Sketches.BuildSketch;
 import Sketches.CountMinSketch;
 import Sketches.CountMinSketchAggregator;
+import Sketches.HyperLogLogSketch;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.common.functions.RichMapFunction;
 import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.api.java.tuple.Tuple4;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.streaming.api.TimeCharacteristic;
@@ -15,66 +18,53 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.LocalStreamEnvironment;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.AssignerWithPunctuatedWatermarks;
+import org.apache.flink.streaming.api.watermark.Watermark;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.util.Collector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nullable;
 import java.io.IOException;
 
 public class CountWindowJob {
     public static void main(String[] args) throws Exception {
+
         // set up the streaming execution environment
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime);
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 
-        //Set up the sketch parameters
+        int keyField = 0;
+
         int width = 10;
         int height = 5;
-        int seed = 1;
+        long seed = 1;
+        Object[] parameters = new Object[]{width,height,seed};
+        Class<CountMinSketch> sketchClass = CountMinSketch.class;
 
-        //Read file, convert to tuple and add the partition index
-        DataStream<String> line = env.readTextFile("data/10percent.csv");
-        SingleOutputStreamOperator<CountMinSketch> distributed = line.flatMap(new FlatMapFunction<String, Tuple2<Integer, Integer>>() {
-            @Override
-            public void flatMap(String value, Collector<Tuple2<Integer, Integer>> out) {
-                String[] tuples = value.split(",");
+//        int logRegNum = 10;
+//        long seed = 1;
+//
+//        Object[] parameters = new Object[]{logRegNum,seed};
+//        Class<HyperLogLogSketch> sketchClass = HyperLogLogSketch.class;
 
-                if (tuples.length == 2) {
+        long windowSize = 20000;
 
-                    Integer key = new Integer(tuples[0]);
-                    Integer val = new Integer(tuples[1]);
+        DataStream<String> line = env.readTextFile("data/timestamped.csv");
+        DataStream<Tuple3<Integer, Integer, Long>> timestamped = line.flatMap(new EventTimeJob.CreateTuplesFlatMap()) // Create the tuples from the incoming Data
+                .assignTimestampsAndWatermarks(new EventTimeJob.CustomTimeStampExtractor()); // extract the timestamps and add watermarks
 
-                    if (key != null && val != null) {
-                        out.collect(new Tuple2<>(key, new Integer(1)));
-                    }
-                }
-            }
-        }).map(new ProcessingTimeJob.AddParallelismRichMapFunction())
-                .keyBy(0)
-                        .timeWindow(Time.seconds(1))
-                                .aggregate(new CountMinSketchAggregator(height, width, seed, 1));
-
-        SingleOutputStreamOperator<CountMinSketch> finalSketches = distributed.timeWindowAll(Time.seconds(1))
-                .reduce(new ReduceFunction<CountMinSketch>() {
-                    @Override
-                    public CountMinSketch reduce(CountMinSketch value1, CountMinSketch value2) throws Exception {
-                        return value1.merge(value2);
-                    }
-                });
-
-        finalSketches.writeAsText("output/processingTimeSketches.txt", FileSystem.WriteMode.OVERWRITE).setParallelism(1);
+        SingleOutputStreamOperator<CountMinSketch> finalSketch = BuildSketch.countBased(timestamped, windowSize, env, sketchClass, parameters, keyField);
+        finalSketch.writeAsText("output/countCountMinSketch.txt", FileSystem.WriteMode.OVERWRITE).setParallelism(1);
 
         env.execute("Flink Streaming Java API Skeleton");
-
     }
 
     /**
      *  Stateful map function to add the parallelism variable
      */
-    public static class AddParallelismRichMapFunction extends RichMapFunction<Tuple2<Integer, Integer>, Tuple3<Integer, Integer, Integer>> {
-
-        private static final Logger LOG = LoggerFactory.getLogger(LocalStreamEnvironment.class);
+    public static class AddParallelismRichFlatMapFunction extends RichMapFunction<Tuple3<Integer, Integer, Long>, Tuple4<Integer, Integer, Integer, Long>> {
 
         ValueState<Integer> state;
 
@@ -102,14 +92,82 @@ public class CountWindowJob {
         }
 
         @Override
-        public Tuple3<Integer, Integer, Integer> map(Tuple2<Integer, Integer> value) throws Exception {
+        public Tuple4<Integer, Integer, Integer, Long> map(Tuple3<Integer, Integer, Long> value) throws Exception {
 
             int currentNode = state.value();
-            int next = currentNode % this.getRuntimeContext().getNumberOfParallelSubtasks();
+            int next = currentNode +1;
+            next = next % this.getRuntimeContext().getNumberOfParallelSubtasks();
             state.update(next);
 
-            return new Tuple3<>(currentNode, value.f0, value.f1);
+            return new Tuple4<>(currentNode, value.f0, value.f1, value.f2);
+
         }
     }
 
+    /**
+     * FlatMap to create Tuples from the incoming data
+     */
+    public static class CreateTuplesFlatMap implements FlatMapFunction<String, Tuple3<Integer, Integer, Long>>{
+        @Override
+        public void flatMap(String value, Collector<Tuple3<Integer, Integer, Long>> out) throws Exception {
+            String[] tuples = value.split(",");
+
+            if(tuples.length == 3) {
+
+                Integer key = new Integer(tuples[0]);
+                Integer val = new Integer(tuples[1]);
+                Long timestamp = new Long(tuples[2]);
+
+                if (key != null && val != null) {
+                    out.collect(new Tuple3<>(key, val, timestamp));
+                }
+            }
+        }
+    }
+
+    /**
+     * The Custom TimeStampExtractor which is used to assign Timestamps and Watermarks for our data
+     */
+    public static class CustomTimeStampExtractor implements AssignerWithPunctuatedWatermarks<Tuple4<Integer, Integer, Integer, Long>> {
+        /**
+         * Asks this implementation if it wants to emit a watermark. This method is called right after
+         * the {@link #extractTimestamp(Tuple4, long)}   method.
+         *
+         * <p>The returned watermark will be emitted only if it is non-null and its timestamp
+         * is larger than that of the previously emitted watermark (to preserve the contract of
+         * ascending watermarks). If a null value is returned, or the timestamp of the returned
+         * watermark is smaller than that of the last emitted one, then no new watermark will
+         * be generated.
+         *
+         * <p>For an example how to use this method, see the documentation of
+         * {@link AssignerWithPunctuatedWatermarks this class}.
+         *
+         * @param lastElement
+         * @param extractedTimestamp
+         * @return {@code Null}, if no watermark should be emitted, or the next watermark to emit.
+         */
+        @Nullable
+        @Override
+        public Watermark checkAndGetNextWatermark(Tuple4<Integer, Integer, Integer, Long> lastElement, long extractedTimestamp) {
+            return new Watermark(extractedTimestamp);
+        }
+
+        /**
+         * Assigns a timestamp to an element, in milliseconds since the Epoch.
+         *
+         * <p>The method is passed the previously assigned timestamp of the element.
+         * That previous timestamp may have been assigned from a previous assigner,
+         * by ingestion time. If the element did not carry a timestamp before, this value is
+         * {@code Long.MIN_VALUE}.
+         *
+         * @param element                  The element that the timestamp will be assigned to.
+         * @param previousElementTimestamp The previous internal timestamp of the element,
+         *                                 or a negative value, if no timestamp has been assigned yet.
+         * @return The new timestamp.
+         */
+        @Override
+        public long extractTimestamp(Tuple4<Integer, Integer, Integer, Long> element, long previousElementTimestamp) {
+            return element.f3;
+        }
+    }
 }
--- src/main/java/Sketches/BuildSketch.java
+++ src/main/java/Sketches/BuildSketch.java
@@ -4,10 +4,12 @@ import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.common.functions.RichMapFunction;
 import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.*;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
 
 import java.io.IOException;
@@ -22,6 +24,24 @@ public final class BuildSketch {
                 .timeWindow(windowTime)
                 .aggregate(agg)
                 .timeWindowAll(windowTime)
+                .reduce(new ReduceFunction<S>() { // Merge all sketches in the global window
+                    @Override
+                    public Sketch reduce(Sketch value1, Sketch value2) throws Exception {
+                        return value1.merge(value2);
+                    }
+                });
+        return reduce;
+    }
+
+    public static <T, S extends Sketch> SingleOutputStreamOperator<S> countBased(DataStream<T> inputStream, long windowSize, StreamExecutionEnvironment env, Class<S> sketchClass, Object[] parameters, int keyField){
+        SketchAggregator agg = new SketchAggregator(sketchClass, parameters, keyField);
+
+        SingleOutputStreamOperator reduce = inputStream
+                .map(new AddParallelismTuple())
+                .keyBy(0)
+                .countWindow(windowSize/env.getParallelism())
+                .aggregate(agg)
+                .countWindowAll(env.getParallelism())
                 .reduce(new ReduceFunction<S>() { // Merge all sketches in the global window
                     @Override
                     public Sketch reduce(Sketch value1, Sketch value2) throws Exception {
