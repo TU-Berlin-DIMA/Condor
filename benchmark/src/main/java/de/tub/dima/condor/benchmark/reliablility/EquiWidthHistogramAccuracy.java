87786b61f1965d17ae50c19246bdd85e3d1513e4
rudip7 <rudipoepsel7rpl@hotmail.com>
2020-11-19 13:58:12 +0100

    benchmark reliability ewui-width

--- benchmark/src/main/java/de/tub/dima/condor/benchmark/reliablility/EquiWidthHistogramAccuracy.java
+++ benchmark/src/main/java/de/tub/dima/condor/benchmark/reliablility/EquiWidthHistogramAccuracy.java
@@ -1,9 +1,17 @@
 package de.tub.dima.condor.benchmark.reliablility;
 
 import de.tub.dima.condor.benchmark.sources.input.NYCTaxiRideSource;
+import de.tub.dima.condor.benchmark.sources.utils.NYCExtractKeyField;
 import de.tub.dima.condor.benchmark.sources.utils.NYCTimestampsAndWatermarks;
+import de.tub.dima.condor.core.synopsis.Sketches.CountMinSketch;
+import de.tub.dima.condor.core.synopsis.WindowedSynopsis;
 import de.tub.dima.condor.flinkScottyConnector.processor.BuildSynopsis;
 import de.tub.dima.condor.core.synopsis.Histograms.EquiWidthHistogram;
+import de.tub.dima.condor.flinkScottyConnector.processor.SynopsisBuilder;
+import de.tub.dima.condor.flinkScottyConnector.processor.configs.BuildConfiguration;
+import de.tub.dima.scotty.core.windowType.TumblingWindow;
+import de.tub.dima.scotty.core.windowType.Window;
+import de.tub.dima.scotty.core.windowType.WindowMeasure;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.tuple.Tuple11;
 import org.apache.flink.core.fs.FileSystem;
@@ -28,47 +36,47 @@ public class EquiWidthHistogramAccuracy {
     public static void main(String[] args) throws Exception {
 
         System.out.println("Equi-width histogram accuracy test");
-//		double bL = (-73.991119 - (-73.965118)) / (double) 100;
-//		System.out.println(bL);
-
         // set up the streaming execution Environment
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 
-		env.setParallelism(Integer.parseInt(args[0]));
-		env.setMaxParallelism(Integer.parseInt(args[0]));
+        // Get the parallelism
+        int parallelism = Integer.parseInt(args[0]);
 
-//        env.setParallelism(1);
-//        env.setMaxParallelism(1);
-
-        Class<EquiWidthHistogram> synopsisClass = EquiWidthHistogram.class;
-
+        // Initialize NYCTaxi DataSource
         DataStream<Tuple11<Long, Long, Long, Boolean, Long, Long, Double, Double, Double, Double, Short>> messageStream = env
                 .addSource(new NYCTaxiRideSource(-1, 200000, new ArrayList<>())).setParallelism(1);
-
 
         final SingleOutputStreamOperator<Tuple11<Long, Long, Long, Boolean, Long, Long, Double, Double, Double, Double, Short>> timestamped = messageStream
                 .assignTimestampsAndWatermarks(new NYCTimestampsAndWatermarks());
 
-//        SingleOutputStreamOperator<String> result = timestamped.flatMap(new Test(-73.991119, -73.965118, 100));
+        // We want to build the equi-width histogram based on the value of field 6 (startLon)
+        SingleOutputStreamOperator<Long> inputStream = timestamped.map(new NYCExtractKeyField(6));
 
-		SingleOutputStreamOperator<EquiWidthHistogram> synopsesStream = BuildSynopsis.timeBased(timestamped, Time.milliseconds(10000),6, synopsisClass, new Object[]{-73.991119, -73.965118, 100});
+        // Set up other configuration parameters
+        Class<EquiWidthHistogram> synopsisClass = EquiWidthHistogram.class;
+        Window[] windows = {new TumblingWindow(WindowMeasure.Time, 10000)};
+        Object[] synopsisParameters = new Object[]{-73.991119, -73.965118, 100};
 
+        BuildConfiguration config = new BuildConfiguration(inputStream, synopsisClass, windows, synopsisParameters, parallelism);
+
+        // Build the synopses
+        SingleOutputStreamOperator<WindowedSynopsis<EquiWidthHistogram>> synopsesStream = SynopsisBuilder.build(env, config);
+
+        //  Get the number of taxi rides with start longitude between [−73.991119, −73.965118]
 		SingleOutputStreamOperator<Integer> result = synopsesStream.flatMap(new queryBucketCounts());
 
-		result.writeAsText("/share/hadoop/EDADS/accuracyResults/ew-histogram_result_"+Integer.parseInt(args[0])+".csv", FileSystem.WriteMode.OVERWRITE).setParallelism(1);
-
-//        result.writeAsText("EDADS/output/indexes.csv", FileSystem.WriteMode.OVERWRITE).setParallelism(1);
+		result.writeAsText("/share/hadoop/EDADS/accuracyResults/ew-histogram_result_"+parallelism+".csv", FileSystem.WriteMode.OVERWRITE).setParallelism(1);
 
         env.execute("Equi-width histogram accuracy test");
     }
 
-    private static class queryBucketCounts implements FlatMapFunction<EquiWidthHistogram, Integer> {
+    private static class queryBucketCounts implements FlatMapFunction<WindowedSynopsis<EquiWidthHistogram>, Integer> {
 
         @Override
-        public void flatMap(EquiWidthHistogram histogram, Collector<Integer> out) throws Exception {
+        public void flatMap(WindowedSynopsis<EquiWidthHistogram> histogram, Collector<Integer> out) throws Exception {
             //estimate the frequencies of all taxiID's [2013000001, 2013013223]
-            int[] counts = histogram.getFrequency();
+            int[] counts = histogram.getSynopsis().getFrequency();
             for (int i = 0; i < counts.length; i++) {
                 out.collect(counts[i]);
             }
